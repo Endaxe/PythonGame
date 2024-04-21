0f5e432417d43823ce7a6a5adb43cf20d5448791
Endaxe <94972517+Endaxe@users.noreply.github.com>
2024-04-21 17:44:48 +0200

    snake movement

--- .venv/main.py
+++ .venv/main.py
@@ -3,14 +3,15 @@ from random import randrange
 
 WINDOW = 1000
 TILE_SIZE = 40
-RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+RANGE = (TILE_SIZE // 4, WINDOW - TILE_SIZE // 4, TILE_SIZE)
 get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
-snake = pg.rect.Rect([0,0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
 segments = [snake.copy()]
-snake_dir = (0,0)
-screen = pg.display.set_mode([WINDOW]* 2)
+snake_dir = (0, 0)
+time, time_step = 0, 110
+screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
 while True:
@@ -18,19 +19,22 @@ while True:
         if event.type == pg.QUIT:
             exit()
         if event.type == pg.KEYDOWN:
-                if event.key == pg.K_w:
-                    snake_dir = (0, -TILE_SIZE)
-                if event.key == pg.K_s:
-                    snake_dir = (0, TILE_SIZE)
-                if event.key == pg.K_a:
-                    snake_dir = (-TILE_SIZE, 0)
-                if event.key == pg.K_d:
-                    snake_dir = (TILE_SIZE, 0)
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
 
-        screen.fill('black')
-        [pg.draw.rect(screen, 'green', segment) for segment in segments]
+    screen.fill('black')
+    [pg.draw.rect(screen, 'green', segment) for segment in segments]
+    time_now = pg.time.get_ticks()
+    if time_now - time > time_step:
+        time = time_now
         snake.move_ip(snake_dir)
         segments.append(snake.copy())
         segments = segments[-length:]
-        pg.display.flip()
-        clock.tick(60)
+    pg.display.flip()
+    clock.tick(60)
