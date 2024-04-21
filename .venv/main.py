5feacaf6f85b6f71cf3b300068890922fb509184
Endaxe <94972517+Endaxe@users.noreply.github.com>
2024-04-21 18:03:35 +0200

    Complete

--- .venv/main.py
+++ .venv/main.py
@@ -11,24 +11,39 @@ length = 1
 segments = [snake.copy()]
 snake_dir = (0, 0)
 time, time_step = 0, 110
+food = snake.copy()
+food.center = get_random_position()
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
+dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w:
+            if event.key == pg.K_w and dirs[pg.K_w]:
                 snake_dir = (0, -TILE_SIZE)
-            if event.key == pg.K_s:
+                dirs = {pg.K_w: 1, pg.K_s: 0, pg.K_a: 1, pg.K_d: 1}
+            if event.key == pg.K_s and dirs[pg.K_s]:
                 snake_dir = (0, TILE_SIZE)
-            if event.key == pg.K_a:
+                dirs = {pg.K_w: 0, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
+            if event.key == pg.K_a and dirs[pg.K_a]:
                 snake_dir = (-TILE_SIZE, 0)
-            if event.key == pg.K_d:
+                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 0}
+            if event.key == pg.K_d and dirs[pg.K_d]:
                 snake_dir = (TILE_SIZE, 0)
-
+                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 0, pg.K_d: 1}
     screen.fill('black')
+    self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
+    if snake.left < 0  or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or self_eating:
+        snake.center, food.center = get_random_position(), get_random_position()
+        length, snake_dir = 1, (0,0)
+        segments = [snake.copy()]
+    if snake.center == food.center:
+        food.center = get_random_position()
+        length += 1
+    pg.draw.rect(screen, 'red', food)
     [pg.draw.rect(screen, 'green', segment) for segment in segments]
     time_now = pg.time.get_ticks()
     if time_now - time > time_step:
